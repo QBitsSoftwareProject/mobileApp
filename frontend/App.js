7454f2dc7e781983bbb3c9dddabe33145a22bb0c
Ravindu Nethsara <139568202+NethsaraWMRC@users.noreply.github.com>
2023-12-31 19:04:35 +0530

    folder structure

---
+++ frontend/App.js
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+import NavContainer from './src/navigation/navigationContainer/NavContainer';
+
+
+export default function App() {
+  return (
+    <NavContainer/>
+  );
+}
---
+++ frontend/src/navigation/navigationContainer/NavContainer.js
@@ -0,0 +1,12 @@
+import { NavigationContainer } from '@react-navigation/native';
+import HomeStack from '../route/HomeStack'
+
+const NavContainer = () => {
+  return (
+    <NavigationContainer>
+        <HomeStack/>
+    </NavigationContainer>
+  )
+}
+
+export default NavContainer
