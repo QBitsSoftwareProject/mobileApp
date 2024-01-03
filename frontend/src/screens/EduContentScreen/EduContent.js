03dcc5283bc61d6cbd42d21bb5bc1f49ef5c913c
darkPixels123 <125457336+darkPixels123@users.noreply.github.com>
2024-01-03 16:13:45 +0530

    created custom search box
    to be developed further

--- frontend/src/screens/EduContentScreen/EduContent.js
+++ frontend/src/screens/EduContentScreen/EduContent.js
@@ -2,11 +2,18 @@ import { View, Text } from "react-native";
 import React from "react";
 import styles from "./style";
 import { SafeAreaView } from "react-native-safe-area-context";
+// search bar
+import SearchBar from "react-native-dynamic-search-bar";
+// search bar
 
 const EduContent = () => {
   return (
     <SafeAreaView>
-      <Text>EduContent</Text>
+      <SearchBar
+        placeholder="Search..."
+        onPress={() => alert("onPress")}
+        onChangeText={(text) => console.log(text)}
+      />
     </SafeAreaView>
   );
 };
