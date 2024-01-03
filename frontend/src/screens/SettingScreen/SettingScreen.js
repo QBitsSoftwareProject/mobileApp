7af52eb889691dad9efeb43446964c74fd6c3cb9
Ravindu Nethsara <139568202+NethsaraWMRC@users.noreply.github.com>
2024-01-03 12:00:37 +0530

    header componenet done

--- frontend/src/screens/SettingScreen/SettingScreen.js
+++ frontend/src/screens/SettingScreen/SettingScreen.js
@@ -1,12 +1,20 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import HeaderSub from '../../components/HeaderSub.js/HeaderSub'
+
 
 const SettingScreen = () => {
   return (
-    <View>
-      <Text>SettingScreen</Text>
-    </View>
+    <SafeAreaView>
+      <HeaderSub headLine={'Goals'} subHeadLine={"\"Self-care is how you take your power back.- Lalah Delia\""}/>
+      <View style={{marginRight:25,marginLeft:25}}>
+        <Text >SettingScreen</Text>
+      </View>
+      
+    </SafeAreaView>
   )
 }
 
 export default SettingScreen
+
