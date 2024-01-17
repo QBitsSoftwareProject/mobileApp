9f58833781931b8894ea0973b235a8292974f1c4
Sakuni1004 <sakunikakarunathilaka99@gmail.com>
2024-01-17 11:57:37 +0530

    first commit

---
+++ frontend/src/screens/JournalScreen/JornalStyles.js
@@ -0,0 +1,6 @@
+import {styleSheet} from "react-native";
+const styles = styleSheet.create({
+
+});
+
+export default styles;
---
+++ frontend/src/screens/JournalScreen/Journal.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { StyleSheet } from 'react-native';
+
+const Journal = () => {
+  return (
+    <View>
+      <Text>Journal</Text>
+    </View>
+  )
+}
+
+export default Journal
