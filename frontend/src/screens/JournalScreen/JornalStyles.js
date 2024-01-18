6bbe7431c006059d8edb4ef49dc02bad1367951b
Sakuni1004 <sakunikakarunathilaka99@gmail.com>
2024-01-18 07:58:23 +0530

    add journal page

--- frontend/src/screens/JournalScreen/JornalStyles.js
+++ frontend/src/screens/JournalScreen/JornalStyles.js
@@ -1,6 +1,156 @@
 import {styleSheet} from "react-native";
 const styles = styleSheet.create({
 
+    container: {
+        flex: 1,
+        backgroundColor: "#F2F3F5",
+      },
+      header: {
+        backgroundColor: "#4A90BF",
+        width:574.83,
+        height:189
+      },
+      title: {
+        fontSize: 32,
+        fontWeight: "bold",
+        color: "#fff",
+        paddingTop:80,
+        paddingLeft:25
+      },
+      subtitle: {
+        fontSize: 18,
+        color: "#fff",
+        paddingTop:7,
+        paddingLeft:25,
+        
+      },
+      buttons: {
+        flexDirection: "row",
+        paddingTop:32,
+        paddingLeft:110,
+        paddingBottom:32
+    
+      },
+      button1: {
+        backgroundColor: "#5296C5",
+        padding: 10,
+        borderRadius: 20,
+        width:112,
+        height:40
+      },
+    
+      button2: {
+        backgroundColor: "#ffffff",
+        padding: 10,
+        borderRadius: 20,
+        height:40,
+        width:112
+      },
+    
+    
+      buttonText1: {
+        textAlign:'center',
+        fontSize: 14,
+        color: "#fff",
+      },
+    
+      buttonText2: {
+        textAlign:'center',
+        fontSize: 14,
+        color: "#5C677D",
+      },
+      feeling: {
+        marginTop:0,
+        marginLeft: 25,
+        
+      },
+      label: {
+        fontSize: 18,
+        fontWeight: 'medium',
+        color: "#101318",
+      },
+      emoji: {
+        fontSize: 32,
+        marginHorizontal: 5,
+        paddingTop:15,
+        paddingBottom:32
+    
+      },
+      input: {
+       paddingTop:0,
+       marginLeft:25,
+       fontSize:18,
+       paddingBottom:32
+    
+      },
+      textInput: {
+    
+        backgroundColor:'#ffffff',
+        borderRadius: 20,
+        paddingLeft:25,
+        fontSize: 14,
+        width:360,
+        height:69,
+        shadowOpacity:0.25,
+        elevation:4,
+        color:'#7D8597'
+      },
+      textArea: {
+        
+        backgroundColor:'#ffffff',
+        borderRadius: 20,
+        paddingLeft:25,
+        fontSize: 14,
+        height: 200,
+        width:360,
+        shadowOpacity:0.25,
+        elevation:4,
+        color:'#7D8597',
+        
+    
+      },
+      stress: {
+        marginHorizontal: 20,
+        marginVertical: 10,
+      },
+      circles: {
+        flexDirection: "row",
+        justifyContent: "space-around",
+        marginVertical: 10,
+        paddingBottom:32
+      },
+      circle: {
+        width: 45,
+        height: 45,
+        borderRadius: 30,
+        alignItems: "center",
+        justifyContent: "center",
+      },
+      stress: {
+        fontSize: 11,
+        fontWeight: 'normal',
+        color: "#40495B",
+        
+        
+      },
+      create: {
+        padding:10,
+        backgroundColor: "#ffffff",
+        borderColor:'#5FA1CE',
+        borderWidth:2,
+        width:289,
+        height:50,
+        borderRadius: 75,
+        marginLeft:58
+        
+      },
+      createText: {
+        paddingTop:3,
+        fontSize: 16,
+        color:'#101318',
+        textAlign:'center'
+      },
+
 });
 
 export default styles;
--- frontend/src/screens/JournalScreen/Journal.js
+++ frontend/src/screens/JournalScreen/Journal.js
@@ -1,13 +1,90 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+
+import React, { useState } from "react";
 import { StyleSheet } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, FlatList, ScrollView } from "react-native";
+
+const emojis = ["😊", "😢", "😡", "😍", "😱", "😐", " 😴" ,"🤒"];
 
 const Journal = () => {
-  return (
-    <View>
-      <Text>Journal</Text>
-    </View>
-  )
-}
+  const [title, setTitle] = useState("");
+  const [entry, setEntry] = useState("");
+  const [feeling, setFeeling] = useState("");
+  const [stress, setStress] = useState("");
 
-export default Journal
+  const handleCreate = () => {
+    //logic for creating a new journal entry here
+    alert("Your journal is ready");
+  };
+
+  const renderEmoji = ({ item }) => {
+    return (
+      <TouchableOpacity onPress={() => setFeeling(item)}>
+        <Text style={styles.emoji}>{item}</Text>
+      </TouchableOpacity>
+    );
+  };
+
+  const renderStress = (level, color) => {
+    return (
+      <TouchableOpacity onPress={() => setStress(level)}>
+        <View style={[styles.circle, { backgroundColor: color }]}>
+          <Text style={styles.stress}>{level}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
+  return (
+    <View style={styles.container}>
+      <ScrollView>
+      <View style={styles.header}>
+        <Text style={styles.title}>Add New Journal</Text>
+        <Text style={styles.subtitle}>Welcome to our mindful haven</Text>
+      </View>
+      <View style={styles.buttons}>
+        <TouchableOpacity style={[styles.button1,{marginRight:0}]}>
+          <Text style={styles.buttonText1}>Create</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.button2,{marginLeft:-30}]}>
+          <Text style={styles.buttonText2}>View</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.feeling}>
+        <Text style={styles.label}>Feeling with...</Text>
+        <FlatList
+          data={emojis}
+          renderItem={renderEmoji}
+          keyExtractor={(item) => item}
+          horizontal={true}
+        />
+      </View>
+      <View style={styles.input}>
+        <Text style={[styles.label,{paddingBottom:15}]}>Journal Title</Text>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Feeling Happy today..."
+          value={title}
+          onChangeText={setTitle}
+        />
+      </View>
+      <View style={styles.input}>
+        <Text style={[styles.label,{paddingBottom:15}]}>Write your entry</Text>
+        <TextInput
+          style={styles.textArea}
+          
+          placeholder ="I have a happy day today. at school...., "
+          multiline={true}
+          value={entry}
+          onChangeText={setEntry}
+        />
+      </View>
+    
+      <TouchableOpacity style={styles.create} onPress={handleCreate}>
+        <Text style={styles.createText}>Create Journal</Text>
+      </TouchableOpacity>
+      </ScrollView>
+    </View>
+  );
+};
+
+export default Journal;
