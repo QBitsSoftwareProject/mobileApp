e468f264cc80dd7a967c5dcb933438bda830365a
Ravindu Nethsara <139568202+NethsaraWMRC@users.noreply.github.com>
2024-01-18 17:24:33 +0530

    red

--- frontend/src/screens/HomeScreen/styles.js
+++ frontend/src/screens/HomeScreen/styles.js
@@ -3,7 +3,7 @@ import { StyleSheet } from 'react-native'
 
 const styles = StyleSheet.create({
   Headertxt : {
-    color:'black',
+    color:'red',
     fontSize: 32,
     fontWeight:"600",
     textAlign:'center',
