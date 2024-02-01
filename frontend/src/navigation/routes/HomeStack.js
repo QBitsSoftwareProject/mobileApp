a37fee05979f494af2c2bffa4fe6ce6e27f37441
Madusha-Weerasinghe <141899939+Madusha-Weerasinghe@users.noreply.github.com>
2024-02-01 18:37:01 +0530

    mind relaxing method suggestion screen created

--- frontend/src/navigation/routes/HomeStack.js
+++ frontend/src/navigation/routes/HomeStack.js
@@ -1,5 +1,6 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from '../../screens/HomeScreen/HomeScreen'
+import Mindrelaxinmethod from '../../screens/MindRelaxingMethodScreen.js/mindrelaxinmethod';
 
 const stack = createNativeStackNavigator();
 
@@ -9,7 +10,8 @@ const HomeStack = () => {
 
     <stack.Navigator screenOptions={{ headerStyle: { flex:1,backgroundColor: 'transparent' }, headerShown:false }}>
       
-      <stack.Screen name='HomeScreen' component={HomeScreen} /> 
+      {/* <stack.Screen name='HomeScreen' component={HomeScreen} />  */}
+      <stack.Screen name='HomeScreen' component={Mindrelaxinmethod} /> 
    
       
     </stack.Navigator>
---
+++ frontend/src/screens/MindRelaxingMethodScreen.js/mindrelaxinmethod.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+export class Mindrelaxinmethod extends Component {
+
+  render() {
+
+    return (
+      <View>
+        <Text> helloo world </Text>
+      </View>
+    )
+
+  }
+
+}
+
+export default Mindrelaxinmethod
