a2ddf6379a5921ad136eae9dc2dc0dc723ac5550
Madusha-Weerasinghe <141899939+Madusha-Weerasinghe@users.noreply.github.com>
2024-02-03 00:35:41 +0530

    update the expand card

--- frontend/src/components/MindRelaxingMethod/ExpandCard.js
+++ frontend/src/components/MindRelaxingMethod/ExpandCard.js
@@ -1,160 +1,155 @@
-import React, { useState, useRef } from 'react';
-import { Text, View, TouchableOpacity, Animated ,Image,StyleSheet,ImageBackground} from 'react-native';
+import React, { useState, useRef, useEffect } from 'react';
+import { Text, View, TouchableOpacity, Animated, Image, StyleSheet, ImageBackground } from 'react-native';
 
-const ExpandableCard = () => {
-  const [isExpanded, setIsExpanded] = useState(false);
+const ExpandableCard = (props) => {
+  const [isExpanded, setIsExpanded] = useState(true);
   const heightAnim = useRef(new Animated.Value(0)).current;
-  const [contentHeight, setContentHeight] = useState(0);
+  const [imageHeight, setImageHeight] = useState(0);
+  const [textHeight, setTextHeight] = useState(0);
 
   const toggleExpand = () => {
     setIsExpanded(!isExpanded);
-    Animated.timing(heightAnim, {
-      toValue: isExpanded ? 0 :250, // Adjust the height as needed
-      duration: 300, // Adjust the duration of the animation
-      useNativeDriver: false,
-    }).start();
   };
 
-//   const onContentLayout = (event) => {
-//     setContentHeight(event.nativeEvent.layout.height);
-//   };
+  const imglink = props.imgLink;
+  console.log(imglink);
+
+  useEffect(() => {
+    Animated.timing(heightAnim, {
+      toValue: isExpanded ? 0 : imageHeight + textHeight,
+      duration: 300,
+      useNativeDriver: false,
+    }).start();
+  }, [isExpanded, imageHeight, textHeight]);
+
+  const onImageLayout = (event) => {
+    setImageHeight(event.nativeEvent.layout.height);
+  };
+
+  const onTextLayout = (event) => {
+    setTextHeight(event.nativeEvent.layout.height);
+  };
 
   return (
     <View style={{ margin: 10 }}>
-      
-        <View
-          style={{
-            backgroundColor: 'white',
-            padding: 10,
-            borderRadius: 20,
-            height:100,
-            flexDirection:'row',
-            height:112
-            
-          }}>
-           
-            <View style = {styles.expandImg}>
-                <Image source={require('../../assets/images/MindRelaxingMethod/mp3.png')}/>
-            </View>
-            <View style = {styles.expandTitle}>
-
-                <View style = {styles.title}>
-                    <Text style = {styles.method} >Listen a music</Text>
-                    <Text style = {styles.methodName}>Felista by Yanni</Text>
-
-                </View>
-
-
-                <View style = {styles.expandIcon}>
-
-
-            <TouchableOpacity onPress={toggleExpand}>
-          {/* <Text style={{ fontSize: 18, fontWeight: 'bold' }}>
-            Click to {isExpanded ? 'Collapse' : 'Expand'}
-          </Text> */}
-          <Image
-        source={
-          isExpanded
-            ? require('../../assets/images/MindRelaxingMethod/Expandup.png')
-            : require('../../assets/images/MindRelaxingMethod/expanddown.png')
-        }
-      />
-          </TouchableOpacity>
-          </View>
-          </View>
-          
-        </View>
-      
-
-      <Animated.View 
+      <View
         style={{
-          overflow: 'hidden',
-          height: heightAnim,
-        
-        //   marginTop: 10,
-        //   padding: 10,
           backgroundColor: 'white',
-          borderRadius: 8,
-          
-        }}>
-        {/* <Text>This is the expanded content of the card.</Text> */}
-        
-        <View style = {{height:160, paddingHorizontal:80}} >
-        <ImageBackground
-        source={require('../../assets/images/MindRelaxingMethod/felista.png')}
-        style = {styles.contentImage}>
-            
-            <TouchableOpacity>
-          <Image source= {require('../../assets/images/MindRelaxingMethod/mp3playbutton.png')}/>
-          </TouchableOpacity>
-      </ImageBackground>
+          padding: 10,
+          borderRadius: isExpanded ? 20 : 20,
+          borderBottomLeftRadius: isExpanded ? 20 : 0,
+          borderBottomRightRadius: isExpanded ? 20 : 0,
+          flexDirection: 'row',
+          height: 112,
+        }}
+      >
+        <View style={styles.expandImg}>
+          <Image source={require('../../assets/images/MindRelaxingMethod/mp3.png')} />
+        </View>
+        <View style={styles.expandTitle}>
+          <View style={styles.title}>
+            <Text style={styles.method}>{props.method}</Text>
+            <Text style={styles.methodName}>{props.methodname}</Text>
+          </View>
+          <View style={styles.expandIcon}>
+            <TouchableOpacity onPress={toggleExpand}>
+              <Image
+                source={
+                  isExpanded
+                    ? require('../../assets/images/MindRelaxingMethod/expanddown.png')
+                    : require('../../assets/images/MindRelaxingMethod/Expandup.png')
+                }
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
       </View>
-      <Text style = {styles.contentText} >"Felitsa" is a heartfelt composition by Yanni from his 1997 album 
-"Tribute," dedicated to his mother. Known for its emotional
-melodies, the track showcases Yanni's unique fusion of world and 
-classical music.</Text>
-        
-        
-      </Animated.View>
-      
+      <View>
+        <Animated.View
+          style={{
+            overflow: 'hidden',
+            height: heightAnim,
+            backgroundColor: 'white',
+            borderBottomLeftRadius: 15,
+            borderBottomRightRadius: 15,
+            marginTop:-15,
+          }}
+        >
+          <View style={{ flexDirection: 'column' }}>
+            <View style={{ height: 160, paddingHorizontal: 80, marginTop:10}} onLayout={onImageLayout}>
+              <View style={{ flex: 1, borderRadius: 15, overflow: 'hidden' }}>
+                <ImageBackground
+                  source={{ uri: imglink }}
+                  style={styles.contentImage}
+                >
+                  <TouchableOpacity>
+                    <Image source={require('../../assets/images/MindRelaxingMethod/mp3playbutton.png')} />
+                  </TouchableOpacity>
+                </ImageBackground>
+              </View>
+            </View>
+            <View onLayout={onTextLayout}>
+              <Text style={styles.contentText}>
+                {props.contentText}
+              </Text>
+            </View>
+          </View>
+        </Animated.View>
+      </View>
     </View>
-    
   );
 };
 
 const styles = StyleSheet.create({
-    expandImg: {
-        flex:1,
-        // backgroundColor:'red',
-        justifyContent:'center',
-        alignItems:'center'
-        
-    },
-
-    expandTitle: {
-        flexDirection:'row',
-        flex:3,
-        
-    },
-
-    title:{
-        flex:9,
-        paddingTop:8,
-        paddingLeft:10
-    },
-
-    expandIcon:{
-        flex:1,
-        paddingTop:8
-    },
-
-    method:{
-        fontSize:18,
-        color:"#101318",
-        marginBottom:5,
-        fontWeight: 'bold',
-    },
-
-    methodName:{
-        fontSize:12,
-        color:"#5C677D"
-    },
-
-    contentImage:{
-        flex: 1,
-        // resizeMode: 'cover', // or 'stretch' or 'contain'
-        alignItems:"center",
-        
-        justifyContent: 'center',
-    },
-    contentText:{
-        marginHorizontal:10,
-        marginTop:10,
-        color:"#5C677D",
-    }
-
-
+  expandImg: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
     
-})
+  },
+
+  expandTitle: {
+    flexDirection: 'row',
+    flex: 3,
+  },
+
+  title: {
+    flex: 9,
+    paddingTop: 8,
+    paddingLeft: 10,
+  },
+
+  expandIcon: {
+    flex: 1,
+    paddingTop: 8,
+  },
+
+  method: {
+    fontSize: 18,
+    color: '#101318',
+    marginBottom: 5,
+    fontWeight: 'bold',
+  },
+
+  methodName: {
+    fontSize: 12,
+    color: '#5C677D',
+  },
+
+  contentImage: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    
+  },
+
+  contentText: {
+    marginHorizontal: 10,
+    marginVertical: 20,
+    color: '#5C677D',
+    fontSize: 12,
+    lineHeight: 15,
+  },
+});
 
 export default ExpandableCard;
--- frontend/src/screens/MindRelaxingMethodScreen.js/mindrelaxinmethod.js
+++ frontend/src/screens/MindRelaxingMethodScreen.js/mindrelaxinmethod.js
@@ -1,18 +1,62 @@
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import React, { useState } from 'react'
+import { Text, View ,FlatList,TouchableOpacity, ScrollView} from 'react-native'
 import HeaderSub from '../../components/HeaderSub/HeaderSub'
 import ExpandableCard from '../../components/MindRelaxingMethod/ExpandCard'
+import data from './dummyData';
 
 const Mindrelaxinmethod =() => {
 
- 
+      const imgurl1 = 'https://firebasestorage.googleapis.com/v0/b/uploadingfile-9e556.appspot.com/o/images%2Fcropped-img_0550.webp87e20a44-2fc9-4528-b7bb-1c120eedddb0?alt=media&token=2e92ba76-135e-4cfb-b4ce-75f0e252930b';
 
+      const renderItem = ({ item,index }) => (
+        <ExpandableCard method={item.title} methodname = {item.name} contentText = {item.description} imgLink = {item.img}
+        laststyle={{ marginBottom: index === data.length - 1 ? 500 : 0 }} />
+      );
+//           
     return (
       <View>
         <HeaderSub headLine = {"Suggestions"} subHeadLine={"Explore personalized recommendations"}/>
-        <ExpandableCard/>
-        <ExpandableCard/>
+        
+        <View>
+      <TouchableOpacity style = {{backgroundColor:'yellow'}}>
+        <Text>hdrf</Text>
+      </TouchableOpacity>
+    </View>
+
+      <ScrollView>
+    {data.map((item, index) => (
+    <ExpandableCard
+      key={item.id}
+      method={item.title}
+      methodname={item.name}
+      contentText={item.description}
+      imgLink={item.img}
+      style={{ marginBottom: index === data.length - 1 ? 500 : 0 }}
+    />
+  ))}
+
+    <View style = {{justifyContent:'center',alignItems:'center'}}>
+      <TouchableOpacity 
+      style = {{backgroundColor:'white',
+      marginBottom:370,
+      width:289,
+      height:58,
+      justifyContent:'center',
+      borderRadius:50,
+      borderColor:'#74A9CD',
+      borderWidth:1,
+      marginTop:22
+
+      
+      }}>
+
+        <Text style = {{alignSelf:'center'}}>Try Again</Text>
+      </TouchableOpacity>
+    </View>
+    </ScrollView>
+    
       </View>
+
     )
 
     }
