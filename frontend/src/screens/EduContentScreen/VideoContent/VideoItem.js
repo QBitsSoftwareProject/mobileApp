b11c4c964ab0627484138a3c644159fc454e73b6
darkPixels123 <125457336+darkPixels123@users.noreply.github.com>
2024-02-10 18:36:47 +0530

    Revert "Revert "Revert "Revert "video-player""""
    This reverts commit e585c8a113eb5f531c15cb99658034ad27dfe389.

---
+++ frontend/src/screens/EduContentScreen/VideoContent/VideoItem.js
@@ -0,0 +1,55 @@
+import { View, Text, TouchableOpacity, Image,SafeAreaView } from 'react-native'
+import React from 'react'
+
+import styles from "./VideoItemStyles";
+
+import defaultImg from "../../../assets/images/videoThumbnails/meditation1.png";
+
+// playbtn
+import playBtn from "../../../assets/images/icons/player/play.png";
+// playbtn
+
+// dependencies
+import VideoPlayer from 'react-native-video-player';
+// dependencies
+
+const VideoItem = ({item}) => {
+  return (
+    <View>
+      {/* video item */}
+      <View style={styles.VideoItem}>
+        <View
+          style={{
+            position: "relative",
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <TouchableOpacity style={styles.playBtn}>
+            <Image source={playBtn} />
+          </TouchableOpacity>
+          <Image
+            source={item.thumbnail_loc}
+            style={{
+              width: "100%",
+              borderTopLeftRadius: 10,
+              borderTopRightRadius: 10,
+              position: "absolute",
+            }}
+          />
+        </View>
+        <View style={styles.VideoItemDetails}>
+          <View style={styles.details1}>
+            <Text style={{ fontWeight: "bold" }}>{item.name}</Text>
+          </View>
+          <View style={styles.details2}>
+            <Text style={{ fontWeight: "bold" }}>10:15</Text>
+          </View>
+        </View>
+      </View>
+      {/* video item */}
+    </View>
+  );
+}
+
+export default VideoItem
