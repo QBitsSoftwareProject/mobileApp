e585c8a113eb5f531c15cb99658034ad27dfe389
darkPixels123 <125457336+darkPixels123@users.noreply.github.com>
2024-02-10 18:35:00 +0530

    Revert "Revert "Revert "video-player"""
    This reverts commit 58c7bf699e364d7edf15e7f526a7bfef36d793d5.

--- frontend/src/screens/EduContentScreen/VideoContent/VideoData.js
+++ frontend/src/screens/EduContentScreen/VideoContent/VideoData.js
@@ -3,55 +3,6 @@ export default [
     id: "1",
     name: "BREATHING MEDITATION",
     thumbnail_loc: require("../../../assets/images/videoThumbnails/meditation1.png"),
-    videoLocation: require("../../../assets/videos/meditation/meditation1.mp4"),
-    tags: [
-      {
-        id: "1",
-        tag: "health",
-      },
-      {
-        id: "2",
-        tag: "meditation",
-      },
-    ],
-  },
-  {
-    id: "2",
-    name: "STRESS REDUCTION",
-    thumbnail_loc: require("../../../assets/images/videoThumbnails/meditation2.png"),
-    videoLocation: require("../../../assets/videos/meditation/meditation2.mp4"),
-    tags: [
-      {
-        id: "1",
-        tag: "health",
-      },
-      {
-        id: "2",
-        tag: "meditation",
-      },
-    ],
-  },
-  {
-    id: "3",
-    name: "ATTRACTION MEDITATION",
-    thumbnail_loc: require("../../../assets/images/videoThumbnails/meditation1.png"),
-    videoLocation: require("../../../assets/videos/meditation/meditation2.mp4"),
-    tags: [
-      {
-        id: "1",
-        tag: "health",
-      },
-      {
-        id: "2",
-        tag: "meditation",
-      },
-    ],
-  },
-  {
-    id: "4",
-    name: "YOGA",
-    thumbnail_loc: require("../../../assets/images/videoThumbnails/yoga1.png"),
-    videoLocation: require("../../../assets/videos/yoga/yoga1.mp4"),
     tags: [
       {
         id: "1",
