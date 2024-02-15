41117e92c5da5d50f5e57895e52dbec148407228
Piyumi-Amarasinha <142490188+Piyumi-Amarasinha@users.noreply.github.com>
2024-02-16 02:20:06 +0530

    update communication-forum feature

--- frontend/src/screens/HomePage/HomePage.js
+++ frontend/src/screens/HomePage/HomePage.js
@@ -1,14 +1,32 @@
-import React, { Component } from 'react'
-import { View, Text, StyleSheet} from 'react-native'
+import { View, Text, StyleSheet, ScrollView} from 'react-native'
 import { SafeAreaView } from 'react-native'
 import HeaderSub from '../../components/HeaderSub/HeaderSub'
+import TimeButton from '../../components/TimeButton/TimeButton'
+import CreateCard from '../../components/Card/CreateCard'
 
 const HomePage = () => {
 
     
     return (
-        <View style={style.contains}>
-            <HeaderSub headLine={'Thishakya Perera'} subHeadLine={'80 total post'} />
+        <View style={{margin:25}}>
+            <View style={style.contains}>
+                <HeaderSub headLine={'Thishakya Perera'} subHeadLine={'80 total post'} />
+            </View>
+        
+            <SafeAreaView>
+                
+                <ScrollView>
+                    <View>
+                        <TimeButton/>
+                    </View>
+
+                    <View>
+                        <CreateCard/>
+                    </View>
+                </ScrollView>
+
+            </SafeAreaView>
+        
         </View>
     )
     
