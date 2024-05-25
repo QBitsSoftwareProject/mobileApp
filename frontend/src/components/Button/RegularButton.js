5f3aed52b5fa714110240bf7adeb6dfcde4a6250
Piyumi-Amarasinha <142490188+Piyumi-Amarasinha@users.noreply.github.com>
2024-05-25 18:17:21 +0530

    test something

--- frontend/src/components/Button/RegularButton.js
+++ frontend/src/components/Button/RegularButton.js
@@ -1,32 +1,31 @@
-import { StyleSheet, View, Text, TouchableOpacity} from 'react-native'
-import React from 'react'
+import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
+import React from "react";
 
 const RegularButton = (props) => {
-    
-    return(
-        <View>
-            <TouchableOpacity style={styles.button} onPress={() => props.onPress()}>
-                <Text style={styles.buttonText}>{props.name}</Text>
-            </TouchableOpacity>
-        </View>
-    )
-}
+  return (
+    <View>
+      <TouchableOpacity style={styles.button} onPress={() => props.onPress()}>
+        <Text style={styles.buttonText}>{props.name}</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
 
-const styles = StyleSheet.create ({
-button: {
-    width:'100%',
-    height:50,
-    backgroundColor: 'red', 
+const styles = StyleSheet.create({
+  button: {
+    width: "100%",
+    height: 50,
+    backgroundColor: "#4A90BF",
     borderRadius: 20,
-    marginVertical:15,
-    alignItems: 'center',
-    justifyContent:'center'
+    marginVertical: 15,
+    alignItems: "center",
+    justifyContent: "center",
   },
   buttonText: {
-    color: '#fff', 
+    color: "#fff",
     fontSize: 16,
-    fontWeight: '600'
+    fontWeight: "600",
   },
-})
+});
 
-export default RegularButton
+export default RegularButton;
