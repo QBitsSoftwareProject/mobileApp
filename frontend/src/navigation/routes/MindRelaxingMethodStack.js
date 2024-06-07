864bc5348e61545dca7ca8cae88ec8f9efd07d92
Madusha-Weerasinghe <141899939+Madusha-Weerasinghe@users.noreply.github.com>
2024-06-07 19:09:05 +0530

    create mindrelaxing methos stack

--- frontend/src/navigation/routes/MindRelaxingMethodStack.js
+++ frontend/src/navigation/routes/MindRelaxingMethodStack.js
@@ -1,22 +1,21 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from '../../screens/HomeScreen/HomeScreen'
-
+import Mindrelaxinmethod from '../../screens/MindRelaxingMethodScreen.js/mindrelaxinmethod';
 
 
 const stack = createNativeStackNavigator();
 
-const HomeStack = () => {
+const MindRelaxingMethodStack = () => {
 
   return (
 
     <stack.Navigator screenOptions={{ headerStyle: { flex:1,backgroundColor: 'transparent' }, headerShown:false }}>
       
-      <stack.Screen name='HomeScreen' component={HomeScreen} /> 
-  
+      <stack.Screen name='HomeScreen' component={Mindrelaxinmethod} /> 
+      
     </stack.Navigator>
 
   )
 }
 
-export default HomeStack
+export default MindRelaxingMethodStack
 
