c1d0ac3d7a7e900c65d9f1d9496edc9a2ad3646c
Sakuni1004 <sakunikakarunathilaka99@gmail.com>
2024-06-15 01:32:12 +0530

    stack

--- frontend/src/navigation/routes/HomeStack.js
+++ frontend/src/navigation/routes/HomeStack.js
@@ -1,35 +1,23 @@
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from '../../screens/HomeScreen/HomeScreen';
-import MoodAnalysis from '../../screens/MoodAnalysisScreen/SelectMood';
-import AnalysisGraph from '../../screens/AnalysisGraphScreen/AnalysisGraph';
-import SuggestioScreen from '../../screens/SuggestionsScreen/SuggestioScreen';
-import MonthAnalysis from '../../screens/MonthAnalysis/MonthAnalysis'
-import MonthlyAnalysisGraph from '../../screens/MonthlyAnalysisGraphScreen/MonthlyAnalysisGraph'
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "../../screens/HomeScreen/HomeScreen";
 
+import MoodAnalysisStack from "./MoodAnalysisStack";
 
 const stack = createNativeStackNavigator();
 
 const HomeStack = () => {
-
   return (
+    <stack.Navigator
+      screenOptions={{
+        headerStyle: { flex: 1, backgroundColor: "transparent" },
+        headerShown: false,
+      }}
+    >
+      <stack.Screen name="HomeScreen" component={HomeScreen} />
 
-    <stack.Navigator screenOptions={{ headerStyle: { flex:1,backgroundColor: 'transparent' }, headerShown:false }}>
-      
-      <stack.Screen name='MonthAnalysisScreen' component={MonthAnalysis} />
-       <stack.Screen name='MoodAnaysisScreen' component={MoodAnalysis} /> 
-      <stack.Screen name='AnalysisGraphScreen' component={AnalysisGraph} />  
-      <stack.Screen name='SuggestionsScreen' component={SuggestioScreen} />
-      <stack.Screen name='MonthlyAnalysisGraphScreen' component={MonthlyAnalysisGraph} />
-
-
-
-   
-      
+      <stack.Screen name="MoodAnalysis" component={MoodAnalysisStack} />
     </stack.Navigator>
-  
+  );
+};
 
-  )
-}
-
-export default HomeStack
-
+export default HomeStack;
