1f75a66febadf6175175e0f0b31ba0b18cc4d90c
Piyumi-Amarasinha <142490188+Piyumi-Amarasinha@users.noreply.github.com>
2024-06-19 17:22:25 +0530

    homeStack

--- frontend/src/navigation/routes/HomeStack.js
+++ frontend/src/navigation/routes/HomeStack.js
@@ -1,11 +1,6 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import HomeScreen from "../../screens/HomeScreen/HomeScreen";
-import PostCategory from "../../screens/PostCategory/PostCategory";
-import CreatePost from "../../screens/CreatePost/CreatePost";
-import PostContent from "../../screens/PostContent/PostContent";
-import HomePage from "../../screens/CommunityHomePage/HomePage";
-import ProfileScreen from "../../screens/CommunityProfileScreen/CommunityProfile";
-import CommentPage from "../../screens/CommentPage/CommentPage";
+
+import CommunityHomeStack from "./CommunityHomeStack";
 
 const stack = createNativeStackNavigator();
 
@@ -18,12 +13,8 @@ const HomeStack = () => {
       }}
     >
       {/* <stack.Screen name='HomeScreen' component={HomeScreen} />  */}
-      <stack.Screen name="HomePage" component={HomePage} />
-      <stack.Screen name="PostCategory" component={PostCategory} />
-      <stack.Screen name="CreatePost" component={CreatePost} />
-      <stack.Screen name="PostContent" component={PostContent} />
-      <stack.Screen name="ProfileScreen" component={ProfileScreen} />
-      <stack.Screen name="CommentPage" component={CommentPage} />
+
+      <stack.Screen name="CommunityStack" component={CommunityHomeStack} />
     </stack.Navigator>
   );
 };
