ee47bd5e77688d0d381ed8d793129f7d301a281c
Madusha-Weerasinghe <141899939+Madusha-Weerasinghe@users.noreply.github.com>
2024-06-30 16:59:38 +0530

    add routes to the queation route

--- backend/src/api/controllers/stressQuestionsController/question.controller.js
+++ backend/src/api/controllers/stressQuestionsController/question.controller.js
@@ -5,23 +5,22 @@ const asyncHandler = require('express-async-handler');
 
 
 const createQuestionWithOptions = asyncHandler(async (req, res) => {
-    const { question, options } = req.body;
+    const { question, imgurl, options } = req.body;
 
     try {
-        // Create Options first
+        // create Options 
         const createdOptions = await OptionsModel.insertMany(options);
 
-        // Extract IDs of created options
+        // take IDs from created options
         const optionIds = createdOptions.map(option => option._id);
 
         // Create Question and associate Option IDs
         const newQuestion = await QuestionModel.create({
             question,
-            options: optionIds // Assign option IDs to the question
+            imgurl,
+            options: optionIds,
+            questionId: '' 
         });
-
-        // Update the question field in each option to reference the new question
-        await OptionsModel.updateMany({ _id: { $in: optionIds } }, { $set: { question: newQuestion._id } });
 
         res.status(201).json({
             message: 'Question created successfully',
@@ -115,11 +114,98 @@ const getOptionDetailsById = asyncHandler(async (req, res) => {
     }
 });
 
+const findQuestionById = asyncHandler(async (req, res) => {
+    const questionId = req.params.id;
+
+    try {
+        const foundQuestion = await QuestionModel.findById(questionId).populate('options');
+
+        if (!foundQuestion) {
+            return res.status(404).json({ error: 'Question not found' });
+        }
+
+        res.status(200).json({
+            message: 'Question found',
+            question: foundQuestion
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+const updateQuestionWithOptionsById = asyncHandler(async (req, res) => {
+    const { id } = req.params; // Get the ID from request parameters
+    const { question, imgurl, options } = req.body;
+
+    try {
+        // Update Options
+        const updatedOptions = await Promise.all(options.map(async (option) => {
+            if (option._id) {
+                // If the option has an ID, update it
+                await OptionsModel.findByIdAndUpdate(option._id, option);
+                return option._id; // Return the ID
+            } else {
+                // If the option doesn't have an ID, create it
+                const createdOption = await OptionsModel.create(option);
+                return createdOption._id; // Return the ID
+            }
+        }));
+
+        // Update Question
+        const updatedQuestion = await QuestionModel.findByIdAndUpdate(id, {
+            question,
+            imgurl,
+            options: updatedOptions,
+            questionId: '' // Assuming you need to set questionId
+        }, { new: true });
+
+        if (!updatedQuestion) {
+            return res.status(404).json({ error: 'Question not found' });
+        }
+
+        res.status(200).json({
+            message: 'Question updated successfully',
+            question: updatedQuestion
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+// delete question by id
+const deleteQuestionById = async (req, res) => {
+
+    const { id } = req.params;
+    
+    try {
+        // Find the question by ID and delete it
+        const deletedQuestion = await QuestionModel.findByIdAndDelete(id);
+    
+        if (!deletedQuestion) {
+          return res.status(404).json({ error: 'Question not found' });
+        }
+    
+        // Delete associated options
+        await OptionsModel.deleteMany({ _id: { $in: deletedQuestion.options } });
+    
+        res.status(200).json({ message: 'Question and associated options deleted successfully' });
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+      }
+    };
+
 module.exports = {
     createQuestionWithOptions,
     getAllQuestions,
     getAllOptions,
     getOptionDetailsById,
     getQuestionById,
-    getAllQuestionIds
+    getAllQuestionIds,
+    findQuestionById,
+    updateQuestionWithOptionsById,
+    deleteQuestionById
+
 };
