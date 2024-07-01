7725623aa5e3a612141488b6df6d38b82784e0ca
darkPixels123 <125457336+darkPixels123@users.noreply.github.com>
2024-07-01 12:28:39 +0530

    get user by month analytics

---
+++ backend/src/api/controllers/regularUserControllers/getUsersByMonth.js
@@ -0,0 +1,71 @@
+const regularUser = require("../../models/regularUser/regularUser");
+
+exports.getUsersByMonth = async (req, res) => {
+    try {
+        const month = parseInt(req.query.month);
+
+        if (isNaN(month) || month < 1 || month > 12) {
+            return res.status(400).json({ message: "Invalid month parameter. Please provide a value between 1 and 12." });
+        }
+
+        // Aggregation pipeline to filter users by the month of registration and group them
+        const usersByMonth = await regularUser.aggregate([
+            {
+                $match: {
+                    $expr: {
+                        $eq: [{ $month: "$createdAt" }, month]
+                    }
+                }
+            },
+            {
+                $group: {
+                    _id: { $month: "$createdAt" },
+                    users: { $push: "$$ROOT" },
+                    count: { $sum: 1 }
+                }
+            }
+        ]);
+
+        res.status(200).json(usersByMonth.length);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Server error" });
+    }
+};
+
+// const RegularUser = require("../../models/regularUser/regularUser");
+
+// exports.getUsersByMonth = async (req, res) => {
+//     try {
+//         const month = parseInt(req.query.month);
+
+//         if (isNaN(month) || month < 1 || month > 12) {
+//             return res.status(400).json({ message: "Invalid month parameter. Please provide a value between 1 and 12." });
+//         }
+
+//         // Aggregation pipeline to filter users by the month of registration and count them
+//         const usersByMonth = await RegularUser.aggregate([
+//             {
+//                 $match: {
+//                     $expr: {
+//                         $eq: [{ $month: "$createdAt" }, month]
+//                     }
+//                 }
+//             },
+//             {
+//                 $group: {
+//                     _id: null, // Grouping by null to get the total count
+//                     count: { $sum: 1 }
+//                 }
+//             }
+//         ]);
+
+//         // Return the count of users
+//         const userCount = usersByMonth.length > 0 ? usersByMonth[0].count : 0;
+//         res.status(200).json({ month, userCount });
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).json({ message: "Server error" });
+//     }
+// };
+
