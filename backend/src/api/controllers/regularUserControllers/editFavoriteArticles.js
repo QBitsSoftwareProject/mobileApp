8edbf37f813c3233d5dfc8dba03010cc4b12b340
darkPixels123 <125457336+darkPixels123@users.noreply.github.com>
2024-07-09 22:00:23 +0530

    add to favorites feature

--- backend/src/api/controllers/regularUserControllers/editFavoriteArticles.js
+++ backend/src/api/controllers/regularUserControllers/editFavoriteArticles.js
@@ -3,11 +3,36 @@ const regularUser = require("../../models/regularUser/regularUser");
 
 exports.editFavoriteArticles = async (req, res) => {
     try {
-        const favoriteArticles = req.body;
+        const { favoriteArticles } = req.body;
         const userId = req.params;
 
         // Finding and updating the user by ID
-        await regularUser.findByIdAndUpdate(userId.id, { favArticles: favoriteArticles });
+        const user = await regularUser.findById(userId.id);
+
+        if (!user) {
+            return res.status(404).json({ message: "user not found" });
+        }
+
+        let updateArticles = [];
+        let count = 0;
+
+        // user.favArticles.push(favoriteArticles);
+        console.log("user favArticles:", user.favArticles);
+        user.favArticles.map((item) => {
+            if (item != favoriteArticles) {
+                updateArticles.push(item);
+            } else {
+                count++;
+            }
+        });
+
+        if (count == 0) {
+            updateArticles.push(favoriteArticles);
+        }
+        console.log("user updated articles:",updateArticles);
+
+        user.favArticles = updateArticles;
+        await user.save();
 
         // Sending success response with status code 201 and a message
         return res
