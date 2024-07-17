9c3520798247b2078d19919cd61e0ceef4daa071
darkPixels123 <125457336+darkPixels123@users.noreply.github.com>
2024-07-17 20:56:41 +0530

    route bug fixing

--- backend/src/api/controllers/goalsControllers/updateController.js
+++ backend/src/api/controllers/goalsControllers/updateController.js
@@ -13,6 +13,45 @@ exports.updateGoal = async (req, res) => {
       objectivesState,
       duration,
       category,
+  
+    } = req.body;
+
+    // Extracting the user ID from request parameters
+    const { id } = req.params;
+
+ 
+
+    // Creating an object with updated user details
+    const updateGoal = {
+      title,
+      subTitle,
+      description,
+      objectives,
+      completness,
+
+      objectivesState,
+      duration,
+      category,
+   
+    };
+
+    // Finding and updating the user by ID
+    await goalModel.findByIdAndUpdate(id, updateGoal);
+
+    // Sending success response with status code 201 and a message
+    return res.status(201).json({ message: "Goal updated successfully" });
+  } catch (err) {
+    res.status(500).json({ error: "Goal update failed", details: err.message });
+  }
+};
+
+
+
+exports.updateGoalRating = async (req, res) => {
+  try {
+    // Destructuring the request body to extract user details
+    const {
+    
       currentRating,
       ratingCount,
     } = req.body;
@@ -39,15 +78,7 @@ exports.updateGoal = async (req, res) => {
 
     // Creating an object with updated user details
     const updateGoal = {
-      title,
-      subTitle,
-      description,
-      objectives,
-      completness,
-
-      objectivesState,
-      duration,
-      category,
+  
       currentRating: newRatingValue.toFixed(2),
       ratingCount: newRatingCount,
     };
@@ -61,3 +92,4 @@ exports.updateGoal = async (req, res) => {
     res.status(500).json({ error: "Goal update failed", details: err.message });
   }
 };
+
